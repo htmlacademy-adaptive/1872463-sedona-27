13aa43ae1840c9ea5d299f81f067faf96be6a62b
Konstantin Zaytsev <kgzaicev@gmail.com>
2023-01-27 21:23:25 +0300

    Защита 1-ая попытка. Внесение правки: 9.

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,10 @@
-let navMain = document.querySelector('.main-nav');
-let navToggle = document.querySelector('.main-nav__toggle');
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
+const header = document.querySelector('.main-header');
 
 navMain.classList.remove('main-nav--nojs');
+navMain.classList.add('main-nav--closed');
+header.classList.remove('main-header--nojs');
 
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-nav--closed')) {
